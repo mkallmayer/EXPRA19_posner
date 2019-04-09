627346a7aed057aa73ca171c0c2e773af056e141
mkallmayer <mel.kallmayer@gmail.com>
2019-04-09 14:51:32 +0200

    cueing plugin progress (base)

--- jspsych/plugins/jspsych-posner-cueing.js
+++ jspsych/plugins/jspsych-posner-cueing.js
@@ -1,30 +1,71 @@
 /*
- * Example plugin template
+ * positionsner Cueing plugin
  */
 
-jsPsych.plugins["PLUGIN-NAME"] = (function() {
+jsPsych.plugins["posner-cueing"] = (function() {
 
   var plugin = {};
 
   plugin.info = {
-    name: "PLUGIN-NAME",
+    name: "posner-cueing",
     parameters: {
-      parameter_name: {
-        type: jsPsych.plugins.parameterType.INT, // BOOL, STRING, INT, FLOAT, FUNCTION, KEYCODE, SELECT, HTML_STRING, IMAGE, AUDIO, VIDEO, OBJECT, COMPLEX
+      condition: {
+        type: jsPsych.plugins.parameterType.INT, // 0 valid 1 invalid 2 neutral
         default: undefined
       },
-      parameter_name: {
-        type: jsPsych.plugins.parameterType.IMAGE,
-        default: undefined
+      detectedKey: {
+        type: jsPsych.plugins.parameterType.INT,
+        default: 32
       }
     }
   }
 
   plugin.trial = function(display_element, trial) {
 
+    display_element.innerHTML = ''; // clear the screen
+
+    var timing = {
+      // all in ms
+      cue_onset: 1000,
+      cue_visible: 50,
+      cue_off_target_on_intervall: 600
+    };
+
+    var positions = {
+      // all in px
+      left_x: window.innerWidth / 5,
+      mid_x: window.innerWidth / 2,
+      right_x: window.innerWidth - (window.innerWidth / 5),
+      y: window.innerHeight / 2
+    };
+
+    // body (background)
+    var body = document.getElementsByClassName("jspsych-display-element")[0];
+    body.style.backgroundColor = "grey";
+
+    // canvas
+    var canvas = document.createElement("canvas");
+    display_element.appendChild(canvas);
+    canvas.width = 0.95 * window.innerWidth; // no-scrollbar workaround
+    canvas.height = 0.95 * window.innerHeight; // no-scrollbar workaround
+
+    // context
+    var ctx = canvas.getContext("2d");
+
+    // draw fixation cross
+    ctx.font = "50pt Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+
+    ctx.fillText("+", positions.mid_x, positions.y);
+
+    function draw(img, x, y) {
+      
+    }
+
     // data saving
     var trial_data = {
-      parameter_name: 'parameter value'
+      rt: -1
     };
 
     // end trial
