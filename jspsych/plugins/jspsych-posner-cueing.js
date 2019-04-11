0fa3cd392ec5e9a97000d3f82378b8442e080d1b
mkallmayer <mel.kallmayer@gmail.com>
2019-04-11 12:09:25 +0200

    scrollbars hidden; positioning done; todo: user input

--- jspsych/plugins/jspsych-posner-cueing.js
+++ jspsych/plugins/jspsych-posner-cueing.js
@@ -34,26 +34,10 @@ jsPsych.plugins["posner-cueing"] = (function() {
 
   plugin.trial = function(display_element, trial) {
 
-    var left = document.createElement("div");
-    left.style.position = "absolute";
-    left.style.left = "10%";
-    left.style.top = "50%";
-
-    var right = document.createElement("div");
-    right.style.position = "absolute";
-    right.style.left = "90%";
-    right.style.top = "50%";
-
-    var center = document.createElement("div");
-    center.style.position = "absolute";
-    center.style.left = "50%";
-    center.style.top = "50%";
-
-    var divs = {
-      0: left,
-      1: center,
-      2: right
-    }
+    var stims = {
+      width: 80,
+      height: 80
+    };
 
     var target_file = {
       0: "img_left.png",
@@ -65,15 +49,49 @@ jsPsych.plugins["posner-cueing"] = (function() {
     var body = document.getElementsByClassName("jspsych-display-element")[0];
     //body.style.backgroundColor = "grey";
 
-    body.appendChild(left);
-    body.appendChild(center);
-    body.appendChild(right);
-
     // canvas
     var canvas = document.createElement("canvas");
     display_element.appendChild(canvas);
-    canvas.width = 0.95 * window.innerWidth; // no-scrollbar workaround
-    canvas.height = 0.95 * window.innerHeight; // no-scrollbar workaround
+    canvas.width = window.innerWidth; // no-scrollbar workaround
+    canvas.height = window.innerHeight; // no-scrollbar workaround
+
+    // create divs for which to draw stims in
+    var left = document.createElement("div");
+    left.style.position = "absolute";
+    left.style.left = ((canvas.width / 10) - (stims.width / 2))+ "px";
+    left.style.top = ((canvas.height / 2) - (stims.height / 2))+ "px";
+    left.style.width = stims.width + "px"
+    left.style.height = stims.height + "px"
+    left.style.background = "black"
+
+    var right = document.createElement("div");
+    right.style.position = "absolute";
+    right.style.left = ((9 * canvas.width / 10) - (stims.width / 2))+ "px";
+    right.style.top = ((canvas.height / 2) - (stims.height / 2))+ "px";
+    right.style.width = stims.width + "px"
+    right.style.height = stims.height + "px"
+    right.style.background = "black"
+
+    var center = document.createElement("div");
+    center.style.position = "absolute";
+    center.style.left = ((canvas.width / 2) - (stims.width / 2))+ "px";
+    center.style.top = ((canvas.height / 2) - (stims.height / 2))+ "px";
+    center.style.width = stims.width + "px"
+    center.style.height = stims.height + "px"
+    center.style.background = "black"
+
+    var divs = {
+      0: left,
+      1: center,
+      2: right
+    };
+
+    console.log(divs);
+
+    // bind divs to body
+    body.appendChild(left);
+    body.appendChild(center);
+    body.appendChild(right);
 
     // context
     var ctx = canvas.getContext("2d");
@@ -82,7 +100,7 @@ jsPsych.plugins["posner-cueing"] = (function() {
       // draws target after time period set in timing.cue_off_target_on_interval and starts watching for keyboard response
       
       jsPsych.pluginAPI.setTimeout(function() {
-        divs[2*trial.target_loc].innerHTML = "<img class='center' src='jspsych/target.png'></img>";
+        divs[2*trial.target_loc].innerHTML = "<img src='jspsych/target.png'></img>";
         // start response thingy
       }, trial.cue_off_target_on_interval);
     }
@@ -99,7 +117,7 @@ jsPsych.plugins["posner-cueing"] = (function() {
         stim_file = target_file[2];
       }
 
-      center.innerHTML = "<img class='center' src='jspsych/"+stim_file+"'></img>";
+      center.innerHTML = "<img src='jspsych/"+stim_file+"'></img>";
 
       jsPsych.pluginAPI.setTimeout(function() {
         center.innerHTML = "";
